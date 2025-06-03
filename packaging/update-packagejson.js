8e2ba7b9eace21461f1c900425a524acc11a8a94
Gauravjeet Singh <gaurav.ishwerdas@gmail.com>
2025-06-03 12:59:53 +0530

    feat: rename build as per branch, push final build to azure storage

--- packaging/update-packagejson.js
+++ packaging/update-packagejson.js
@@ -6,15 +6,11 @@ const packageJson = require('../package.json');
 const teamId = process.env.APPLE_TEAM_ID;
 
 if (!teamId) {
-  console.error('TEAM_ID is not defined in the environment variables');
-  process.exit(0);
+  console.error('APPLE_TEAM_ID is not defined in the environment variables');
+  process.exit(1);
 }
 
-if (!process.env.I_AM_TRAVIS) {
-  console.log('Not running on Travis CI. Skipping update.');
-  process.exit(0);
-}
-
+// Update the team ID in package.json
 packageJson.build.mac.notarize.teamId = teamId;
 
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2), 'utf-8');
